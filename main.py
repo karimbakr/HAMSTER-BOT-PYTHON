06a27d9f39906df6d87b932f135273370bc1634e
karimbakr <68517536+karimbakr@users.noreply.github.com>
2024-10-19 17:24:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-# main.py
 import json
 import time
 import threading
+import os
 from account_handler import handle_account, update_balance, get_token_for_account
-from printer import print_info
+from printer import print_info, print_success, print_error
 
-# قراءة الرؤوس من ملف JSON
+# Check if the upgrades_response.json file exists, and create it if it doesn't
+file_path = 'upgrades_response.json'
+if not os.path.exists(file_path):
+    with open(file_path, 'w') as file:
+        pass  # Create the file without content
+
+# Read headers from JSON file
 with open('headers.json', 'r') as headers_file:
     headers = json.load(headers_file)
 
-# قراءة قائمة الترقيات من ملف نصي
+# Read upgrade IDs from a text file
 with open('upgrade_ids.txt', 'r') as file:
     upgrade_ids = [line.strip() for line in file.readlines() if line.strip()]
 
-# التشغيل التلقائي لكل حساب
+# Automatic execution for each account
 if __name__ == "__main__":
     while True:
         with open('init_data.txt', 'r') as file:
@@ -30,20 +36,18 @@ if __name__ == "__main__":
         for thread in threads:
             thread.join()
 
-        # تحديث الرصيد كل 5 دقائق خلال فترة الانتظار
-        for i in range(1):
-            #print_info(f"انتظار تحديث الرصيد بعد 5 دقائق ({i+1}/12)")
-            time.sleep(3)
+        # Update balance every 3 seconds during the wait period
+        print_info("Please wait 3 seconds before updating the balance...")
+        time.sleep(3)
 
-            # تمرير كل من token و headers إلى update_balance
-            for init_data_raw in init_data_list:
-                init_data_raw = init_data_raw.strip()
-                if init_data_raw:
-                    # استخدام نفس الـ token الذي تم الحصول عليه لكل حساب
-                    token = get_token_for_account(init_data_raw, headers)
-                    if token:
-                        thread = threading.Thread(target=update_balance, args=(token, headers))
-                        thread.start()
+        # Pass both token and headers to update_balance
+        for init_data_raw in init_data_list:
+            init_data_raw = init_data_raw.strip()
+            if init_data_raw:
+                token = get_token_for_account(init_data_raw, headers)
+                if token:
+                    thread = threading.Thread(target=update_balance, args=(token, headers))
+                    thread.start()
 
-        print_info("Please wait 1 hour ")
+        print_info("Please wait for 1 hour...")
         time.sleep(3600)
